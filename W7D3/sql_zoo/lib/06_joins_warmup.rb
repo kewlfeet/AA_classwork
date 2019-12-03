1a6df923f61cc23285bf600b7786b4b59d1f4f5f
kewlfeet <ayesh98@gmail.com>
2019-12-02 19:19:03 -0600

    Completed 06_joins_warmup

--- W7D3/sql_zoo/lib/06_joins_warmup.rb
+++ W7D3/sql_zoo/lib/06_joins_warmup.rb
@@ -48,6 +48,12 @@ end
 def year_of_kane
   # Give year of 'Citizen Kane'.
   execute(<<-SQL)
+  SELECT 
+    yr
+  FROM
+    movies
+  WHERE
+    title = 'Citizen Kane'
   SQL
 end
 
@@ -56,36 +62,90 @@ def trek_films
   # these movies include the words Star Trek in the title). Order results by
   # year.
   execute(<<-SQL)
+  SELECT
+    id, title, yr
+  FROM
+    movies
+  WHERE
+    title LIKE 'Star Trek%'
+  ORDER BY yr
   SQL
 end
 
 def films_by_id
   # What are the titles of the films with id 1119, 1595, 1768?
   execute(<<-SQL)
+  SELECT
+    title
+  FROM
+    movies
+  WHERE
+    id = ANY(ARRAY[1119, 1595, 1768])
   SQL
 end
 
 def glenn_close_id
   # What id number does the actress 'Glenn Close' have?
   execute(<<-SQL)
+  SELECT
+    id
+  FROM
+    actors
+  WHERE
+    name = 'Glenn Close'
   SQL
 end
 
 def casablanca_id
   # What is the id of the film 'Casablanca'?
   execute(<<-SQL)
+  SELECT
+    id
+  FROM
+    movies
+  WHERE
+    title = 'Casablanca'
   SQL
 end
 
 def casablanca_cast
   # Obtain the cast list for 'Casablanca'. Use the id value that you obtained
   # in the previous question directly in your query (for example, id = 1).
+  check_id = casablanca_id
   execute(<<-SQL)
+  SELECT 
+    name
+  FROM
+    actors
+  LEFT OUTER JOIN 
+    castings ON actors.id = castings.actor_id
+  WHERE
+    castings.movie_id = (SELECT
+        id
+      FROM
+        movies
+      WHERE
+        title = 'Casablanca'
+    )
   SQL
 end
 
 def alien_cast
   # Obtain the cast list for the film 'Alien'
   execute(<<-SQL)
+  SELECT 
+    name
+  FROM
+    actors
+  LEFT OUTER JOIN 
+    castings ON actors.id = castings.actor_id
+  WHERE
+    castings.movie_id = (SELECT
+        id
+      FROM
+        movies
+      WHERE
+        title = 'Alien'
+    )
   SQL
 end
