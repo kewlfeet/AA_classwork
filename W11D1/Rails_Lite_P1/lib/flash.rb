11e206158c03051bf47d50651621b3da883c428b
kewlfeet <ayesh98@gmail.com>
2019-12-30 18:03:13 -0600

    Rails Lite
    Completed until Flash implementation

--- W11D1/Rails_Lite_P1/lib/flash.rb
+++ W11D1/Rails_Lite_P1/lib/flash.rb
@@ -1,4 +1,29 @@
 require 'json'
 
 class Flash
+attr_accessor :now
+  def initialize(req)
+
+    @flash = {}
+    if req.cookies["_rails_lite_app_flash"]
+      @now = JSON.parse(req.cookies["_rails_lite_app_flash"])
+    else 
+      @now = {}
+    end
+  end
+
+  def [](key)
+    current = @now[key.to_s]
+    # current = @now[key.to_sym] unless current
+    return @flash[key.to_s] unless current
+    current
+  end
+
+  def []=(key, val)
+    @flash[key.to_s] = val
+  end
+
+  def store_flash(res)
+    res.set_cookie("_rails_lite_app_flash", {path: "/" , value: @flash.to_json})
+  end
 end
--- W11D1/Rails_Lite_P1/lib/show_exceptions.rb
+++ W11D1/Rails_Lite_P1/lib/show_exceptions.rb
@@ -2,14 +2,17 @@ require 'erb'
 
 class ShowExceptions
   def initialize(app)
+
   end
 
   def call(env)
+
   end
 
   private
 
   def render_exception(e)
+    
   end
 
 end
