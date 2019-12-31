9bb7e0d467f86eacd5bea00bad1f14f207286719
kewlfeet <ayesh98@gmail.com>
2019-12-30 18:03:42 -0600

    Music App Practice and Continuation

--- W9D5/Music_App/app/controllers/application_controller.rb
+++ W9D5/Music_App/app/controllers/application_controller.rb
@@ -1,2 +1,30 @@
 class ApplicationController < ActionController::Base
+
+    helper_methods :current_user, :logged_in?
+
+    def current_user
+        return nil unless session[:session_token]
+        @current_user ||= User.find_by(session_token: session[:session_token])
+    end
+
+    def require_no_user!
+        redirect_to #something if current_user
+
+    def logged_in?
+        !!current_user
+    end
+
+    def login_user!(user)
+        session[:session_token] = user.reset_session_token!
+    end
+
+    def logout_user!
+        current_user.reset_session_token!
+        session[:session_token] = nil
+    end
+
+    def require_user!
+        redirect_to new_session_url if current_user.nil?
+    end
+
 end
